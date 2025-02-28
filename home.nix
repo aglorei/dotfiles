07ab4b83cf655efd2e216d45cda3ec551a8deee7
Tienlong Pham <tienlong.pham@slalom.com>
2025-02-28 15:01:09 -0600

    Add tool installs, lock

--- home.nix
+++ home.nix
@@ -18,7 +18,17 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [
+    # Shell
+    pkgs.zsh
+
+    # Tools
+    pkgs.git
+    pkgs.fd
     pkgs.jq
+    pkgs.ripgrep
+    pkgs.tmux
+    pkgs.tree
+    pkgs.unzip
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
