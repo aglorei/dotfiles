52edc40029d186a087bf4c34b85a4afa879fdc58
Tienlong Pham <tienlong.pham@slalom.com>
2025-02-28 14:52:04 -0600

    Install jq and initialize lock

--- home.nix
+++ home.nix
@@ -17,7 +17,9 @@
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
-  home.packages = [];
+  home.packages = [
+    pkgs.jq
+  ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
