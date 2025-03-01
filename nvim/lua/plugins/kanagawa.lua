930ea542e4a7cfc01aba638e3e2f5747f406d50a
Tienlong Pham <tienlong.pham@slalom.com>
2025-02-28 18:16:38 -0600

    Configure kanagawa-dragon theme

--- nvim/lua/plugins/kanagawa.lua
+++ nvim/lua/plugins/kanagawa.lua
@@ -1,12 +1,12 @@
 return {
-  -- add gruvbox
+  -- add kanagawa
   { "rebelot/kanagawa.nvim" },
 
-  -- Configure LazyVim to load gruvbox
+  -- Configure LazyVim to load kanagawa
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "kanagawa",
+      colorscheme = "kanagawa-dragon",
     },
   },
 }
