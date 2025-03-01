0afb961402383ca7d40b08b24bc166e16a9dd55a
Tienlong Pham <tienlong.pham@slalom.com>
2025-03-01 12:14:36 -0600

    Make source comment plain

--- home.nix
+++ home.nix
@@ -9,11 +9,7 @@
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
   # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "24.11"; # Please read the comment before changing.
+  home.stateVersion = "24.11";
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
